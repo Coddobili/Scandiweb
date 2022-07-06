049adb1221ec041011820270c2b8f797d0d099c9
Coddobili <lashagagnidze56047@gmail.com>
2022-07-06 14:20:41 +0400

    1

--- Frontend/products-list/src/Components/Header.js
+++ Frontend/products-list/src/Components/Header.js
@@ -1,10 +1,10 @@
 function Header() {
     return (
         <header>
-            <h1 id={'heading'}>Product List</h1>
-            <div id={'buttons'}>
-                <button type={'button'} id={'add'}>ADD</button>
-                <button type={'button'} id={'mass-delete'}>MASS DELETE</button>
+            <h1>Product List</h1>
+            <div>
+                <button type={'button'} id={'add-product-btn'}>ADD</button>
+                <button type={'button'} id={'delete-product-btn'}>MASS DELETE</button>
             </div>
         </header>
     );
--- Frontend/products-list/src/Components/Product.js
+++ Frontend/products-list/src/Components/Product.js
@@ -1,12 +1,16 @@
 function Product(props) {
     return (
       <div id={'product'}>
-          <input type={'checkbox'}/>
+        <div>
+          <input type={'checkbox'} className={'delete-checkbox'}/>
+        </div>
+        <div style={{textAlign: 'center'}}>
           <p>{props.id}</p>
           <p>{props.sku}</p>
           <p>{props.name}</p>
           <p>{props.price}</p>
           <p>{props.about}</p>
+        </div>
       </div>
     );
 }

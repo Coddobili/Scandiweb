429f46d7caab015c98a7d9c868ca5260d3f5c2e5
Coddobili <lashagagnidze56047@gmail.com>
2022-07-07 23:21:34 +0400

    final

--- Backend/get.php
+++ Backend/get.php
@@ -3,6 +3,7 @@ require_once 'Database.php';
 
 header('Access-Control-Allow-Origin: http://localhost:3000');
 header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Methods: GET');
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     $db = new Database();
